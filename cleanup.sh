61b5a8198ea65e5df2f29aa8f6c8d5cc028d1f54
Mitchell Davies <mitchellbdavies@gmail.com>
2024-05-13 18:21:18 -0500

    Added an initial rollout functionality for upgrading the remaining servers.

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,6 @@ podman kill server_1
 podman kill server_2
 podman kill server_3
 podman kill server_4
-podman kill server_5
+podman kill canary
 podman kill load_balancer
 podman network rm load_balanced_net
